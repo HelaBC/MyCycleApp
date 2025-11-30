7c111d6436c882fbcb8a7eb46a7014d49fae9143
HelaBC <helabenchikha0@gmail.com>
2025-11-30 20:20:48 +0100

    loll3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -38,6 +38,11 @@ dependencies {
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
+    implementation("com.github.prolificinteractive:material-calendarview:2.0.0") {
+        exclude( group= "com.android.support", module= "support-v4")
+        exclude( group= "com.android.support", module= "support-compat")
+    }
+    implementation("com.jakewharton.threetenabp:threetenabp:1.4.6")
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
--- app/src/main/java/tn/rnu/isi/mycycle/MainActivity.java
+++ app/src/main/java/tn/rnu/isi/mycycle/MainActivity.java
@@ -1,4 +1,14 @@
 package tn.rnu.isi.mycycle;
 
-public class MainActivity {
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
 }
